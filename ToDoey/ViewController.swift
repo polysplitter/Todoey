ce03771de9995ce32bfa8966684f65dde71a0c5d
Kyle <polysplitter@gmail.com>
2018-08-18 10:44:02 -0500

    Initial Commit

---
+++ ToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Kyle Hansen on 8/18/18.
+//  Copyright © 2018 proxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
